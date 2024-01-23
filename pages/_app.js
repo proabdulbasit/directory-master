4796a25313b4b43d5042f556b7f9890874207b97
proabdulbasit <proabdulbasit.me@gmail.com>
2024-01-24 00:42:59 +0500

    fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
+import { useEffect } from 'react';
+import { setupAnalytics } from "../analytics"
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    setupAnalytics();
+  }, []);
   return<Component {...pageProps} />
   
 }
